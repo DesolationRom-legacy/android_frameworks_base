fce7fbd853df51d808b7a8b5cd9681cdc0e02afe
Danesh M <daneshm90@gmail.com>
2015-01-27 21:17:42 -0800

    SystemUI : Navbar ring targets
    Change-Id: I113d9d257ffa0cb5a51823bee37bd86bb7f19527
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/AndroidManifest.xml
    	packages/SystemUI/res/values/cm_colors.xml
    	packages/SystemUI/res/values/cm_strings.xml
    
    SystemUI : Improve nav ring swipe behavior
    
    Restore stock nav ring swipe behavior.
    
    Change-Id: I28758e7297dc15d72f70ddccc6ccb169fe894388
    
    SystemUI : Move navring app loading to background tasks
    
    Change-Id: I0dc2f2f204e744ada5818ff0e34c3d0c98f84992

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1165,7 +1165,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     @Override
     public void showSearchPanel() {
-        if (mSearchPanelView != null && mSearchPanelView.isAssistantAvailable()) {
+        if (mSearchPanelView != null) {
             mSearchPanelView.show(true, true);
         }
     }
