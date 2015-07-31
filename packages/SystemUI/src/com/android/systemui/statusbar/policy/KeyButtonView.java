eeaa87d5b340c8a4106706230e0eb252651b6b12
Victor Lapin <neonproject@gmail.com>
2015-08-01 01:38:06 +0300

    SystemUI: SlimDim performance boost
    some code optimization to make SlimDim run smoother and get rid
    of the lags
    
    patchset 1: reduced findViewById calls, wrapped search for parent
                view in a Runnable, removed keyguard checks
    patchset 3: made onNavButtonTouched called after home button
                transition ends, instead of every button
    patchset 5: switching battery saver on now disables SlimDim
    
    Change-Id: Ifa831d640518d215675dc621afcef2f507e408ea
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -30,6 +30,7 @@ import android.graphics.drawable.Drawable;
 import android.hardware.input.InputManager;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.SystemClock;
@@ -82,6 +83,8 @@ public class KeyButtonView extends ImageView {
     private KeyButtonRipple mRipple;
     private PowerManager mPm;
     private LongClickCallback mCallback;
+
+    private final Handler mHandler = new Handler();
 
     private final Runnable mCheckLongPress = new Runnable() {
         public void run() {
@@ -294,15 +297,23 @@ public class KeyButtonView extends ImageView {
                 break;
         }
 
-        ViewParent parent = getParent();
-        while (parent != null && !(parent instanceof NavigationBarView)) {
-            parent = parent.getParent();
-        }
-        if (parent != null) {
-            ((NavigationBarView) parent).onNavButtonTouched();
-        }
+        mHandler.post(mNavButtonDimActivator);
+
         return true;
     }
+
+    private final Runnable mNavButtonDimActivator = new Runnable() {
+        @Override
+        public void run() {
+            ViewParent parent = getParent();
+            while (parent != null && !(parent instanceof NavigationBarView)) {
+                parent = parent.getParent();
+            }
+            if (parent != null) {
+                ((NavigationBarView) parent).onNavButtonTouched();
+            }
+        }
+    };
 
     public void playSoundEffect(int soundConstant) {
         mAudioManager.playSoundEffect(soundConstant, ActivityManager.getCurrentUser());
