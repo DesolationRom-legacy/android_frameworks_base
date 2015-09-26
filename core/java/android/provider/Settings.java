54dc362bb2c826d37fb4ddd9c2d71c7e0a4816d7
Snuzzo <jrracinfan@gmail.com>
2015-09-26 13:58:49 -0400

    (1/2) BootAnimation; Store summary in settings provider
    Change-Id: Iaa99f3e1fe2530ec1852f449abdf08b2d3fe64fd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3999,6 +3999,12 @@ public final class Settings {
         public static final String STATUS_BAR_DESO_LOGO = "status_bar_deso_logo";
 
         /**
+         * Selected boot animation summary
+         * @hide
+         */
+        public static final String SET_BOOTANIMATION_KEY = "select_bootanimation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
